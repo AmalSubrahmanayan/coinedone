baa2847e0f9a1abacf9fb966a871d421fe11c2bf
amal <amalsubru@gmail.com>
2023-03-12 21:19:43 +0530

    get and post

--- lib/view/home_screen/home_screen.dart
+++ lib/view/home_screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:coinedone/blocs/controller.dart';
 import 'package:coinedone/constants/colors/app_colors.dart';
 import 'package:coinedone/view/widgets/bottomsheet.dart';
@@ -5,9 +7,30 @@ import 'package:coinedone/view/widgets/calendar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dash/flutter_dash.dart';
 
-class HomeScreen extends StatelessWidget {
+import '../../constants/sizedboxes/app_sizedboxes.dart';
+import '../../model/phone_get_model/phone_get_model.dart';
+import '../../model/phone_post_model/phine_post_model.dart';
+import '../../service/phone_get_service.dart';
+import '../../service/phone_post_service.dart';
+
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final TextEditingController namecontroller = TextEditingController();
+
+  final TextEditingController starttimecontroller = TextEditingController();
+
+  final TextEditingController endtimecontroller = TextEditingController();
+
+  final TextEditingController datecontroller = TextEditingController();
+
+  final TextEditingController phonenumbetcontroller = TextEditingController();
+  List<Datum> userDetails = [];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -51,14 +74,21 @@ class HomeScreen extends StatelessWidget {
                             Padding(
                               padding: const EdgeInsets.all(9.0),
                               child: RichText(
-                                text: const TextSpan(
-                                  text: '6:00 AM - 7:00 AM\n',
+                                text: TextSpan(
+                                  text: userDetails[index].startTime,
                                   style: TextStyle(
                                     color: AppColors.blackColor,
                                   ),
                                   children: [
                                     TextSpan(
-                                      text: 'Morning routine',
+                                      text: '${userDetails[index].endTime}\n',
+                                      style: TextStyle(
+                                        color: AppColors.blackColor,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    TextSpan(
+                                      text: userDetails[index].name,
                                       style: TextStyle(
                                         color: AppColors.blackColor,
                                         fontWeight: FontWeight.w500,
@@ -71,7 +101,7 @@ class HomeScreen extends StatelessWidget {
                           ],
                         );
                       },
-                      itemCount: 5,
+                      itemCount: userDetails.length,
                       separatorBuilder: (BuildContext context, int index) {
                         return Row(
                           children: const [
@@ -93,7 +123,7 @@ class HomeScreen extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.only(left: 300, bottom: 10),
                 child: FloatingActionButton.small(
-                  onPressed: ()async {
+                  onPressed: () async {
                     // 'alert' is the AlertDialog defined in your code
                     // showDialog(
                     //   context: context,
@@ -104,7 +134,122 @@ class HomeScreen extends StatelessWidget {
                     showModalBottomSheet<void>(
                       context: context,
                       builder: (BuildContext context) {
-                        return BottenSheet();
+                        return Container(
+                          height: 700,
+                          padding: const EdgeInsets.all(16),
+                          color: AppColors.whiteColor,
+                          child: SingleChildScrollView(
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceBetween,
+                                  children: [
+                                    const Text(
+                                      "  Add Schedule",
+                                      style: TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 18,
+                                        color: AppColors.blueColor,
+                                      ),
+                                    ),
+                                    IconButton(
+                                      icon: const Icon(Icons.close),
+                                      onPressed: () => Navigator.pop(context),
+                                    )
+                                  ],
+                                ),
+                                AppSizedBoxes.sizedboxH10,
+                                const Text(
+                                  "  Name",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                AppSizedBoxes.sizedboxH5,
+                                Container(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 16),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.bgblueColor,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: TextFormField(
+                                    controller: namecontroller,
+                                    decoration: const InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: "Name",
+                                    ),
+                                  ),
+                                ),
+                                AppSizedBoxes.sizedboxH10,
+                                const Text(
+                                  "  Date and time",
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                AppSizedBoxes.sizedboxH10,
+                                Container(
+                                  // margin: const EdgeInsets.symmetric(vertical: 0  ),
+                                  padding: const EdgeInsets.all(12),
+                                  decoration: BoxDecoration(
+                                    color: AppColors.bgblueColor,
+                                    borderRadius: BorderRadius.circular(10),
+                                  ),
+                                  child: Column(
+                                    children: [
+                                      BottomContainer(
+                                        controller: starttimecontroller,
+                                        text: 'Start Time',
+                                      ),
+                                      // const Divider(
+                                      //   color: AppColors.darkgreyColor,
+                                      // ),
+                                      BottomContainer(
+                                        text: 'End Time',
+                                        controller: endtimecontroller,
+                                      ),
+                                      // const Divider(
+                                      //   color: AppColors.darkgreyColor,
+                                      // ),
+                                      BottomContainer(
+                                        text: 'DAte',
+                                        controller: datecontroller,
+                                      ),
+                                      // const Divider(
+                                      //   color: AppColors.darkgreyColor,
+                                      // ),
+                                      BottomContainer(
+                                        text: 'Phone number',
+                                        controller: phonenumbetcontroller,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                AppSizedBoxes.sizedboxH10,
+                                Center(
+                                  child: ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      minimumSize: const Size(350, 50),
+                                      elevation: 0,
+                                    ),
+                                    // style: ButtonStyle(maximumSize: ),
+                                    child: const Text('Add Schedule'),
+
+                                    onPressed: () async {
+                                      await posting();
+                                      await getting();
+                                      log('posting finished');
+                                      Navigator.of(context).pop();
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
                       },
                     );
 
@@ -121,5 +266,28 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  Future<void> posting() async {
+    PhoneModel model = PhoneModel(
+      name: namecontroller.text,
+      startTime: starttimecontroller.text,
+      endTime: endtimecontroller.text,
+      date: datecontroller.text,
+      phoneNumber: phonenumbetcontroller.text,
+    );
+    PhoneModelService().savePhoneModel(model);
+  }
+
+  Future<void> getting() async {
+    final List<Datum> response =
+        await PhoneService().getData(phonenumbetcontroller.text);
+    if (response != null) {
+      log('enetering');
+      setState(() {
+        userDetails.addAll(response);
+      });
+      log("my list= ${userDetails.toString()}");
+    }
   }
 }
--- lib/view/widgets/bottomsheet.dart
+++ lib/view/widgets/bottomsheet.dart
@@ -1,130 +1,171 @@
-import 'package:coinedone/blocs/controller.dart';
-import 'package:coinedone/constants/colors/app_colors.dart';
-import 'package:coinedone/constants/sizedboxes/app_sizedboxes.dart';
-import 'package:flutter/material.dart';
+// import 'dart:developer';
 
-class BottenSheet extends StatelessWidget {
-  BottenSheet({super.key});
-  final TextEditingController namecontroller = TextEditingController();
-  final TextEditingController starttimecontroller = TextEditingController();
-  final TextEditingController endtimecontroller = TextEditingController();
-  final TextEditingController datecontroller = TextEditingController();
-  final TextEditingController phonenumbetcontroller = TextEditingController();
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 700,
-      padding: const EdgeInsets.all(16),
-      color: AppColors.whiteColor,
-      child: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Text(
-                  "  Add Schedule",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 18,
-                    color: AppColors.blueColor,
-                  ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.close),
-                  onPressed: () => Navigator.pop(context),
-                )
-              ],
-            ),
-            AppSizedBoxes.sizedboxH10,
-            const Text(
-              "  Name",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            AppSizedBoxes.sizedboxH5,
-            Container(
-              padding: const EdgeInsets.symmetric(horizontal: 16),
-              decoration: BoxDecoration(
-                color: AppColors.bgblueColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: TextFormField(
-                controller: namecontroller,
-                decoration: const InputDecoration(
-                  border: InputBorder.none,
-                  hintText: "Name",
-                ),
-              ),
-            ),
-            AppSizedBoxes.sizedboxH10,
-            const Text(
-              "  Date and time",
-              style: TextStyle(
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            AppSizedBoxes.sizedboxH10,
-            Container(
-              // margin: const EdgeInsets.symmetric(vertical: 0  ),
-              padding: const EdgeInsets.all(12),
-              decoration: BoxDecoration(
-                color: AppColors.bgblueColor,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                children: [
-                  BottomContainer(
-                    controller: starttimecontroller,
-                    text: 'Start Time',
-                  ),
-                  // const Divider(
-                  //   color: AppColors.darkgreyColor,
-                  // ),
-                  BottomContainer(
-                    text: 'End Time',
-                    controller: endtimecontroller,
-                  ),
-                  // const Divider(
-                  //   color: AppColors.darkgreyColor,
-                  // ),
-                  BottomContainer(
-                    text: 'DAte',
-                    controller: datecontroller,
-                  ),
-                  // const Divider(
-                  //   color: AppColors.darkgreyColor,
-                  // ),
-                  BottomContainer(
-                    text: 'Phone number',
-                    controller: phonenumbetcontroller,
-                  ),
-                ],
-              ),
-            ),
-            AppSizedBoxes.sizedboxH10,
-            Center(
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: const Size(350, 50),
-                  elevation: 0,
-                ),
-                // style: ButtonStyle(maximumSize: ),
-                child: const Text('Add Schedule'),
+// import 'package:coinedone/blocs/controller.dart';
+// import 'package:coinedone/constants/colors/app_colors.dart';
+// import 'package:coinedone/constants/sizedboxes/app_sizedboxes.dart';
+// import 'package:coinedone/model/phone_get_model/phone_get_model.dart';
+// import 'package:coinedone/service/phone_get_service.dart';
+// import 'package:flutter/material.dart';
 
-                onPressed: () {
-                  posting();
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// import '../../model/phone_post_model/phine_post_model.dart';
+// import '../../service/phone_post_service.dart';
+
+// class BottenSheet extends StatefulWidget {
+//   BottenSheet({super.key});
+
+//   @override
+//   State<BottenSheet> createState() => _BottenSheetState();
+// }
+
+// class _BottenSheetState extends State<BottenSheet> {
+//   final TextEditingController namecontroller = TextEditingController();
+
+//   final TextEditingController starttimecontroller = TextEditingController();
+
+//   final TextEditingController endtimecontroller = TextEditingController();
+
+//   final TextEditingController datecontroller = TextEditingController();
+
+//   final TextEditingController phonenumbetcontroller = TextEditingController();
+// List<Datum> userDetails =[];
+//   @override
+//   Widget build(BuildContext context) {
+//     return Container(
+//       height: 700,
+//       padding: const EdgeInsets.all(16),
+//       color: AppColors.whiteColor,
+//       child: SingleChildScrollView(
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.start,
+//           children: [
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//               children: [
+//                 const Text(
+//                   "  Add Schedule",
+//                   style: TextStyle(
+//                     fontWeight: FontWeight.bold,
+//                     fontSize: 18,
+//                     color: AppColors.blueColor,
+//                   ),
+//                 ),
+//                 IconButton(
+//                   icon: const Icon(Icons.close),
+//                   onPressed: () => Navigator.pop(context),
+//                 )
+//               ],
+//             ),
+//             AppSizedBoxes.sizedboxH10,
+//             const Text(
+//               "  Name",
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             AppSizedBoxes.sizedboxH5,
+//             Container(
+//               padding: const EdgeInsets.symmetric(horizontal: 16),
+//               decoration: BoxDecoration(
+//                 color: AppColors.bgblueColor,
+//                 borderRadius: BorderRadius.circular(10),
+//               ),
+//               child: TextFormField(
+//                 controller: namecontroller,
+//                 decoration: const InputDecoration(
+//                   border: InputBorder.none,
+//                   hintText: "Name",
+//                 ),
+//               ),
+//             ),
+//             AppSizedBoxes.sizedboxH10,
+//             const Text(
+//               "  Date and time",
+//               style: TextStyle(
+//                 fontWeight: FontWeight.bold,
+//               ),
+//             ),
+//             AppSizedBoxes.sizedboxH10,
+//             Container(
+//               // margin: const EdgeInsets.symmetric(vertical: 0  ),
+//               padding: const EdgeInsets.all(12),
+//               decoration: BoxDecoration(
+//                 color: AppColors.bgblueColor,
+//                 borderRadius: BorderRadius.circular(10),
+//               ),
+//               child: Column(
+//                 children: [
+//                   BottomContainer(
+//                     controller: starttimecontroller,
+//                     text: 'Start Time',
+//                   ),
+//                   // const Divider(
+//                   //   color: AppColors.darkgreyColor,
+//                   // ),
+//                   BottomContainer(
+//                     text: 'End Time',
+//                     controller: endtimecontroller,
+//                   ),
+//                   // const Divider(
+//                   //   color: AppColors.darkgreyColor,
+//                   // ),
+//                   BottomContainer(
+//                     text: 'DAte',
+//                     controller: datecontroller,
+//                   ),
+//                   // const Divider(
+//                   //   color: AppColors.darkgreyColor,
+//                   // ),
+//                   BottomContainer(
+//                     text: 'Phone number',
+//                     controller: phonenumbetcontroller,
+//                   ),
+//                 ],
+//               ),
+//             ),
+//             AppSizedBoxes.sizedboxH10,
+//             Center(
+//               child: ElevatedButton(
+//                 style: ElevatedButton.styleFrom(
+//                   minimumSize: const Size(350, 50),
+//                   elevation: 0,
+//                 ),
+//                 // style: ButtonStyle(maximumSize: ),
+//                 child: const Text('Add Schedule'),
+
+//                 onPressed: () async {
+//                   await posting();
+//                  await getting();
+//                   log('posting finished');
+//                   Navigator.of(context).pop();
+//                 },
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+
+//   Future<void> posting() async {
+//     PhoneModel model = PhoneModel(
+//       name: namecontroller.text,
+//       startTime: starttimecontroller.text,
+//       endTime: endtimecontroller.text,
+//       date: datecontroller.text,
+//       phoneNumber: phonenumbetcontroller.text,
+//     );
+//     PhoneModelService().savePhoneModel(model);
+//   }
+//   Future<void>getting()async{
+//     final List<Datum> response = await PhoneService().getData(phonenumbetcontroller.text);
+//     if(response!=null){
+//       userDetails.addAll(response);
+
+//     }
+//   //   final data = await PhoneService(). getData(phonenumbetcontroller.text);
+//   //   log(data.toString());
+//   }
+// }
 
 // class BottomContainer extends StatelessWidget {
 //   const BottomContainer(
@@ -154,25 +195,25 @@ class BottenSheet extends StatelessWidget {
 //   }
 // }
 
+import 'package:coinedone/constants/colors/app_colors.dart';
+import 'package:flutter/material.dart';
+
 class BottomContainer extends StatelessWidget {
   BottomContainer({super.key, required this.text, required this.controller});
   final String text;
   final TextEditingController controller;
-  final GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  // final GlobalKey<FormState> formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
-    return Form(
-      key: formKey,
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          hintText: text,
-          suffixIcon: const Icon(Icons.arrow_forward_ios),
-          enabledBorder: const UnderlineInputBorder(
-            borderSide: BorderSide(
-              color: AppColors.darkgreyColor,
-            ),
+    return TextField(
+      controller: controller,
+      decoration: InputDecoration(
+        hintText: text,
+        suffixIcon: const Icon(Icons.arrow_forward_ios),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(
+            color: AppColors.darkgreyColor,
           ),
         ),
       ),
